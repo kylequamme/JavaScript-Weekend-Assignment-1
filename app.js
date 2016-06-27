9b4ebcb268c298c3b4ee4f59d829586151c01a23
Kyle R. Quamme <kyle.quamme@gmail.com>
2016-06-27 07:41:52 -0500

    finished core assigment

--- app.js
+++ app.js
@@ -10,9 +10,28 @@ var claim5 = new Claim("Steve Wright", "Primary Care", 770);
 
 var initialList = [claim1, claim2, claim3, claim4, claim5]
 
+var claim6 = new Claim("Davey Allison", "Emergency", 11000);
+
+var claim7 = new Claim("Joey Logano", "Primary Care", 550);
+
+var claim8 = new Claim("Kevin Harvick", "Optical", 250)
+
+var claim9 = new Claim("Tony Stewart", "Specialist", 25000)
+
+var claim10 = new Claim("Kyle Busch", "Specialist", 50000)
+
+var secondList = [claim6, claim7, claim8, claim9, claim10]
+
+var claimsList = initialList.concat(secondList);
+
 var totalPayedOut = 0;
 
-console.log(amountCovered(claim1));
+for(var i = 0; i < claimsList.length; i++){
+	totalPayedOut += amountCovered(claimsList[i])
+}
+console.log(Math.round(totalPayedOut));
+
+// for testing amountCovered(claim1);
 
 function Claim(name, type, cost){
 	this.patientName = name;
@@ -37,5 +56,7 @@ function percentCovered(c){
 }
 //function to determine amount covered
 function amountCovered(c) {
-	return c.visitCost * (percentCovered(c) / 100)
+	var costCovered = c.visitCost * (percentCovered(c) / 100)
+	console.log('Paid out $' + costCovered + ' for ' + c.patientName);
+	return costCovered;
 }
